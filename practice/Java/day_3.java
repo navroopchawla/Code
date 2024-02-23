89722810661b92ee0b879aa377c1a90079fcd17e
navroopchawla <navroopchawla5@gmail.com>
2024-02-23 12:28:03 +0530

    1

--- practice/Java/day_3.java
+++
@@ -1,5 +0,0 @@
-public class day_3 {
-    public static void main(String[] args) {
-        
-    }
-}
--- practice/Java/day_4.java
+++
@@ -1,7 +0,0 @@
-public class day_4 {
-    public static void main(String[] args) {
-        // String name = new String("hello");
-        String name="hello";
-        System.out.println(name);
-    }
-}
